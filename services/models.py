caaf92d35742ac2ae84fb23b56103cfa79e45d93
snahmd <sanahmed7@gmail.com>
2020-02-20 19:34:51 +0100

    adding service listing page

--- services/models.py
+++ services/models.py
@@ -17,6 +17,11 @@ class ServiceListingPage(Page):
     FieldPanel('subtitle'),
   ]
 
+  def get_context(self, request, *args, **kwargs):
+    context = super().get_context(request, *args, **kwargs)
+    context['services'] = ServicePage.objects.live().public()
+    return context
+
 class ServicePage(Page):
   
   description = models.TextField(
