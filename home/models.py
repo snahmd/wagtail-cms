5849ac23cf62ff3b69b112601d070f3b18e213de
snahmd <sanahmed7@gmail.com>
2020-02-21 23:56:24 +0100

    adding testimonial component

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
 from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.snippets.blocks import SnippetChooserBlock
 
 from streams import blocks
 
@@ -43,6 +44,10 @@ class HomePage(Page):
         ("cards", blocks.CardsBlock()),
         ("image_and_text", blocks.ImageAndTextBlock()),
         ("cta", blocks.CallToActionBlock()),
+        ("testimonial", SnippetChooserBlock(
+            target_model='testimonials.Testimonial',
+            template="streams/testimonial_block.html",
+        )),
     ], null=True, blank=True)
 
     content_panels = Page.content_panels + [
