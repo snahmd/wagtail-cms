cbe93a4e565899e1b1bf9287957592b6c1bcd5c0
snahmd <sanahmed7@gmail.com>
2020-02-21 01:54:44 +0100

    adding debug toolbar and flex page

--- rocketman/settings/base.py
+++ rocketman/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'home',
     'search',
     'services',
+    'flex',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
--- rocketman/urls.py
+++ rocketman/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import include, path
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -20,12 +21,16 @@ urlpatterns = [
 
 
 if settings.DEBUG:
+    import debug_toolbar
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls))
+    ] + urlpatterns
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
