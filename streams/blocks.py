94b8f3ead8e499869af0f35837ce9b64321eb253
snahmd <sanahmed7@gmail.com>
2020-02-21 15:50:55 +0100

    modified cards block

--- streams/blocks.py
+++ streams/blocks.py
@@ -1,6 +1,7 @@
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
 
+
 class TitleBlock(blocks.StructBlock):
   text = blocks.CharBlock(
     required=True,
@@ -14,20 +15,57 @@ class TitleBlock(blocks.StructBlock):
     help_text = "Centered text to display on the page"
 
 
+class Link(blocks.StructBlock):
+  link_text= blocks.CharBlock(
+    max_length=50, 
+    default='More Details',
+  )
+  internal_page = blocks.PageChooserBlock(
+    required=False
+  )
+  external_link = blocks.URLBlock(
+    required=False
+  )
+    
+
+class Card(blocks.StructBlock):
+  title = blocks.CharBlock(
+    max_length=100,
+    help_text="Bold title text for this card. Max length of 100 characters.",
+  )
+  text = blocks.TextBlock(
+    max_length=255,
+    help_text="Optional text for this card. Max length of 255 characters.",
+    required=False
+  )
+  image = ImageChooserBlock(
+    help_text="Image will be automatically cropped 570px by 370px"
+  )
+  link = Link(help_text="Enter a link or select a page")
+ 
+
 class CardsBlock(blocks.StructBlock):
     cards = blocks.ListBlock(
-      blocks.StructBlock(
-        [
-          ("title", blocks.CharBlock(max_length=100,
-          help_text="Bold title text for this card. Max length of 100 characters.")),
-          ("text", blocks.TextBlock(max_length=255,
-          help_text="Optional text for this card. Max length of 255 characters.", required=False)),
-          ("image", ImageChooserBlock(help_text="Image will be automatically cropped 570px by 370px"))
-        ]
-      )
+
+
+        Card()
+
     )  
 
     class Meta:
       template = "streams/cards_block.html"
       icon = "image"
       label = "Standard Cards"
+
+      #blocks.StructBlock(
+      #  [
+      #    ("title", blocks.CharBlock(max_length=100,
+      #    help_text="Bold title text for this card. Max length of 100 characters.")),
+      #    ("text", blocks.TextBlock(max_length=255,
+      #    help_text="Optional text for this card. Max length of 255 characters.", required=False)),
+      #    ("image", ImageChooserBlock(help_text="Image will be automatically cropped 570px by 370px")),
+      #    ("link_text", blocks.CharBlock(max_length=50, default='More Detail')),
+      #    ("internal_page", blocks.PageChooserBlock(required=False)),
+      #    ("external_link", blocks.URLBlock(required=False)),
+      #  ]
+     # )
