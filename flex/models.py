eec94e3e496ddf6038226a8c227cd9d2d8088694
snahmd <sanahmed7@gmail.com>
2020-02-22 02:52:41 +0100

    adding richtext streamfield

--- flex/models.py
+++ flex/models.py
@@ -1,6 +1,34 @@
 from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.snippets.blocks import SnippetChooserBlock
+from wagtail.core import blocks as wagtail_blocks
+
+from streams import blocks
+from home.models import new_table_options
 
 class FlexPage(Page):
+  body = StreamField([
+        ('title', blocks.TitleBlock()),
+        ("cards", blocks.CardsBlock()),
+        ("image_and_text", blocks.ImageAndTextBlock()),
+        ("cta", blocks.CallToActionBlock()),
+        ("testimonial", SnippetChooserBlock(
+            target_model='testimonials.Testimonial',
+            template="streams/testimonial_block.html",
+        )),
+        ("pricing_table", blocks.PricingTableBlock(table_options=new_table_options)), 
+        ("richtext", wagtail_blocks.RichTextBlock(
+            template="streams/simple_richtext_block.html",
+            features=["h1", "h2", "h3", "h4", "h5", "h6", "bold", "italic",
+             "ol", "ul", "hr", "link", "document-link", "image", "embed", 
+             "code", "blockquote"]
+        )),
+  ], null=True, blank=True)
+
+  content_panels = Page.content_panels + [
+    StreamFieldPanel("body"),
+  ]
 
   class Meta:
     verbose_name = "Flex (misc) page"
