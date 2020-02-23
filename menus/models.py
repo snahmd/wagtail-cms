6148c9ebad0b4dff19ed696d0ef678435505632a
snahmd <sanahmed7@gmail.com>
2020-02-23 02:39:06 +0100

    adding menu pages to footer

--- menus/models.py
+++ menus/models.py
@@ -29,6 +29,25 @@ class MenuItem(Orderable):
 
   page = ParentalKey("Menu", related_name="menu_items")
 
+  @property
+  def link(self) -> str:
+    if self.link_page:
+      return self.link_page.url
+    elif self.link_url:
+      return self_link_url
+    return "#"  
+
+
+  @property 
+  def title(self):
+    if self.link_page and not self.link_title:
+      return self.link_page.title
+    elif self.link_title:
+      return self.link_title
+    return "Missing Title" 
+    
+      
+
 class Menu(ClusterableModel):
   title = models.CharField(max_length=100)
   slug = AutoSlugField(
