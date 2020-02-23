b79c0d3e972bfbc42644a800fe58dd1045aeb5d3
snahmd <sanahmed7@gmail.com>
2020-02-23 03:54:16 +0100

    adding contact forms

--- home/models.py
+++ home/models.py
@@ -36,7 +36,9 @@ new_table_options = {
 
 
 class HomePage(Page):
-    
+    parent_page_types = ["wagtailcore.Page"]
+    subpage_types = ["flex.FlexPage", "services.ServiceListingPage", "contact.ContactPage"]
+    max_count = 1
     lead_text = models.CharField(
         max_length=200,
         blank=True,
--- rocketman/settings/base.py
+++ rocketman/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'testimonials',
     'streams',
     'menus',
+    'contact',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
